f1aa94896e6d48217240a76a4fb26eef80a296f5
Thomas Ralph <tralph@websterbrothers.com>
2019-03-09 18:59:25 -0500

    Finished Reset Component

--- components/ResetPassword.php
+++ components/ResetPassword.php
@@ -10,7 +10,7 @@ use RainLab\User\Components\ResetPassword as ParentReset;
 use RainLab\User\Models\User as UserModel;
 use ValidationException;
 use Validator;
-use phpDocumentor\Reflection\Types\String_;
+use Illuminate\Support\Facades\Redirect;
 
 class ResetPassword extends ParentReset
 {
@@ -26,6 +26,14 @@ class ResetPassword extends ParentReset
     {
         $properties = parent::defineProperties();
         
+        
+        $properties['redirect'] = [
+            'title'       => /*Redirect to*/'webbro.userforms::lang.components.deactivate.redirect_to',
+            'description' => /*Suggested redirect page is the login page*/'webbro.userforms::lang.components.deactivate.redirect_to_desc',
+            'type'        => 'dropdown',
+            'default'     => ''
+        ];
+        
         $properties['showTitles'] = [
             'title'       => /*Show titles*/'webbro.userforms::lang.components.login.show_titles_title',
             'description' => /*Should the field titles be displayed on the field*/'webbro.userforms::lang.components.login.show_titles_desc',
@@ -37,22 +45,27 @@ class ResetPassword extends ParentReset
             'title'       => /*Show header*/'webbro.userforms::lang.components.resetpassword.show_header_title',
             'description' => /*Should the message header be displayed*/'webbro.userforms::lang.components.resetpassword.show_header_desc',
             'type'        => 'checkbox',
-            'default'     => 0
+            'default'     => 1
         ];
         
         $properties['showMessage'] = [
             'title'       => /*Show message*/'webbro.userforms::lang.components.resetpassword.show_message_title',
             'description' => /*Should the message body be displayed*/'webbro.userforms::lang.components.resetpassword.show_message_desc',
             'type'        => 'checkbox',
-            'default'     => 0
+            'default'     => 1
         ];
         
         return $properties;
     }
     
+    public function getRedirectOptions()
+    {
+        return [''=>'- refresh page -', '0' => '- no redirect -'] + Page::sortBy('baseFileName')->lists('baseFileName', 'baseFileName');
+    }
+    
     public function onRun()
     {
-        $this->addJs('assets/js/login.js');
+        $this->addJs('assets/js/forms.js');
         $this->addCss('assets/css/style.css');
         
         $this->prepareVars();
@@ -61,6 +74,10 @@ class ResetPassword extends ParentReset
     public function prepareVars()
     {
         $this->page['showTitles'] = $this->property('showTitles');
+        $this->page['showHeader'] = $this->property('showHeader');
+        $this->page['showMessage'] = $this->property('showMessage');
+        $this->page['header'] = $this->messageHeader();
+        $this->page['body'] = $this->messageBody();
     }
     
     /*
@@ -135,6 +152,11 @@ class ResetPassword extends ParentReset
             throw new ApplicationException(Lang::get(/*A user was not found with the given credentials.*/'rainlab.user::lang.account.invalid_user'));
         }
         
+        if (!$user->is_activated)
+        {
+            throw new ApplicationException('User has not been activated');
+        }
+        
         $code = implode('!', [$user->id, $user->getResetPasswordCode()]);
         
         $link = $this->makeResetUrl($code);
@@ -148,5 +170,89 @@ class ResetPassword extends ParentReset
         Mail::send($this->restoreMailTemplate(), $data, function($message) use ($user) {
             $message->to($user->email, $user->full_name);
         });
+    }
+    
+    /**
+     * Perform the password reset
+     */
+    public function onResetPassword()
+    {
+        try 
+        {
+            $rules = [
+                'code'     => 'required',
+                'password' => 'required|between:4,255'
+            ];
+            
+            $validation = Validator::make(post(), $rules);
+            if ($validation->fails()) {
+                throw new ValidationException($validation);
+            }
+            
+            $message = ['code' => Lang::get(/*Invalid activation code supplied.*/'rainlab.user::lang.account.invalid_activation_code')];
+            
+            /*
+             * Break up the code parts
+             */
+            $parts = explode('!', post('code'));
+            if (count($parts) != 2) {
+                throw new ValidationException($message);
+            }
+            
+            list($userId, $code) = $parts;
+            
+            if (!strlen(trim($userId)) || !strlen(trim($code)) || !$code) {
+                throw new ValidationException($message);
+            }
+            
+            if (!$user = Auth::findUserById($userId)) {
+                throw new ValidationException($message);
+            }
+            
+            if (!$user->attemptResetPassword($code, post('password'))) {
+                throw new ValidationException($message);
+            }
+            
+            $message = "Password reset complete, you may now sign in.";
+            
+            /*
+             * Redirect
+             */
+            if ($redirect = $this->makeRedirection(true))
+            {
+                return $redirect->with('message', $message);
+            }
+        }
+        catch (Exception $ex) 
+        {
+            if (Request::ajax()) throw $ex;
+            else Flash::error($ex->getMessage());
+        }
+    }
+    
+    /*
+     * Helpers
+     */
+    
+    /**
+     * Redirect to the intended page after successful update, sign in or registration.
+     * The URL can come from the "redirect" property or the "redirect" postback value.
+     * @return mixed
+     */
+    protected function makeRedirection($intended = false)
+    {
+        $method = $intended ? 'intended' : 'to';
+        
+        $property = $this->property('redirect');
+        
+        if (!strlen($property)) {
+            return;
+        }
+        
+        $redirectUrl = $this->pageUrl($property) ?: $property;
+        
+        if ($redirectUrl = post('redirect', $redirectUrl)) {
+            return Redirect::$method($redirectUrl);
+        }
     }
 }
